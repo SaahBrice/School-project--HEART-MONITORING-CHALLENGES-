8070fe7d05d06529bb4cc3ebfd744815f830d71a
SaahBrice <saahbrice98@gmail.com>
2024-06-04 19:28:48 +0100

    Final touches

--- hrv_lab/main.py
+++ hrv_lab/main.py
@@ -2,11 +2,14 @@ import argparse
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+import plotly.express as px
+from plotly.subplots import make_subplots
 from plot import plot_series, plot_ecg_signal
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-epoch_folder', '--epoch_folder', type=str, default='./epoch_0/')
+parser.add_argument('-epoch_folder', '--epoch_folder', type=str, default='./epoch_1/')
 parser.add_argument('-window_duration', '--window_duration', type=float, default=240.0)
 parser.add_argument('-window_shift', '--window_shift', type=float, default=60.0)
 parser.add_argument('-extract_ecg', '--extract_ecg', dest='extract_ecg', action='store_true')
@@ -69,22 +72,54 @@ if __name__ == '__main__':
         else:
             print("QRS peak times file not found.")
         
-    if plot_nni:
-        nni_path = os.path.join(epoch_folder, 'nni.npz')
-        sd_path = os.path.join(epoch_folder, 'sd.npz')
-        nni_times_path = os.path.join(epoch_folder, 'nni_times.npz')
-        sd_times_path = os.path.join(epoch_folder, 'sd_times.npz')
+if plot_nni:
+    nni_path = os.path.join(epoch_folder, 'nni.npz')
+    sd_path = os.path.join(epoch_folder, 'sd.npz')
+    nni_times_path = os.path.join(epoch_folder, 'nni_times.npz')
+    sd_times_path = os.path.join(epoch_folder, 'sd_times.npz')
+    
+    if os.path.exists(nni_path) and os.path.exists(sd_path) and os.path.exists(nni_times_path) and os.path.exists(sd_times_path):
+        nn_intervals = np.load(nni_path)['data']
+        subsequent_differences = np.load(sd_path)['data']
+        nni_times = np.load(nni_times_path)['data']
+        sd_times = np.load(sd_times_path)['data']
         
-        if os.path.exists(nni_path) and os.path.exists(sd_path) and os.path.exists(nni_times_path) and os.path.exists(sd_times_path):
-            nn_intervals = np.load(nni_path)['data']
-            subsequent_differences = np.load(sd_path)['data']
-            nni_times = np.load(nni_times_path)['data']
-            sd_times = np.load(sd_times_path)['data']
-            
-            plot_series(nni_times, nn_intervals, 'NNI [s]')        
-            plot_series(sd_times, subsequent_differences, 'SD [s]')
-        else:
-            print("NNI or SD data files not found.")
+        fig = make_subplots(
+            rows=3, cols=2, 
+            column_widths=[0.8, 0.2],
+            subplot_titles=('NN Intervals', 'Subsequent Differences', 'Seizure Detection')
+        )
+
+        fig.add_trace(go.Scatter(x=nni_times, y=nn_intervals, mode='lines', name='NNI'), row=1, col=1)
+        fig.add_trace(go.Scatter(x=sd_times, y=subsequent_differences, mode='lines', name='SD'), row=2, col=1)
+
+        mean_nni = np.mean(nn_intervals)
+        std_nni = np.std(nn_intervals)
+        threshold = mean_nni - 4 * std_nni
+
+        seizure_indices = np.where(nn_intervals < threshold)[0]
+        seizure_times = nni_times[seizure_indices]
+
+        
+
+        fig.add_trace(go.Scatter(x=nni_times, y=nn_intervals, mode='lines', name='NNI'), row=3, col=1)
+        for index in seizure_indices:
+            fig.add_trace(go.Scatter(x=[nni_times[index]], y=[nn_intervals[index]], mode='markers', marker=dict(color='red'), showlegend=False), row=3, col=1)
+
+
+
+        fig.update_layout(height=900, width=1200, title_text="NN Intervals and Seizure Detection")
+        fig.update_xaxes(title_text="Time [s]", row=1, col=1)
+        fig.update_xaxes(title_text="Time [s]", row=2, col=1)
+        fig.update_xaxes(title_text="Time [s]", row=3, col=1)
+        fig.update_yaxes(title_text="NNI [s]", row=1, col=1)
+        fig.update_yaxes(title_text="SD [s]", row=2, col=1)
+        fig.update_yaxes(title_text="NNI [s]", row=3, col=1)
+
+        fig.show()
+    else:
+        print("NNI or SD data files not found.")
+
         
     if compute_hrv:
         nni_path = os.path.join(epoch_folder, 'nni.npz')
@@ -134,15 +169,25 @@ if __name__ == '__main__':
             avg_nn_intervals = np.load(avg_nn_intervals_path)['data']
             std_nn_intervals = np.load(std_nn_intervals_path)['data']
             
-            plot_series(window_times, 60 / np.asarray(avg_nn_intervals), 'HR [beat/min]')
-            plot_series(window_times, avg_nn_intervals, 'AVNN [s]')
-            plot_series(window_times, std_nn_intervals, 'SDNN [s]')
+            fig = make_subplots(rows=3, cols=1, subplot_titles=('HR [beat/min]', 'AVNN [s]', 'SDNN [s]'))
+
+            fig.add_trace(go.Scatter(x=window_times, y=60 / np.asarray(avg_nn_intervals), mode='lines', name='HR'), row=1, col=1)
+            fig.add_trace(go.Scatter(x=window_times, y=avg_nn_intervals, mode='lines', name='AVNN'), row=2, col=1)
+            fig.add_trace(go.Scatter(x=window_times, y=std_nn_intervals, mode='lines', name='SDNN'), row=3, col=1)
+
+            fig.update_layout(height=900, width=1200, title_text="HRV Metrics")
+            fig.update_xaxes(title_text="Time [s]")
+            fig.update_yaxes(title_text="HR [beat/min]", row=1, col=1)
+            fig.update_yaxes(title_text="AVNN [s]", row=2, col=1)
+            fig.update_yaxes(title_text="SDNN [s]", row=3, col=1)
+
+            fig.show()
         else:
             print("HRV data files not found. Please compute HRV first.")
     
     if plot_comparison and compute_nni:
         # Identify and filter out noise
-        noise_threshold = 1.5
+        noise_threshold = 0.9
         filtered_indices = [i for i, interval in enumerate(original_nn_intervals) if interval <= noise_threshold]
         
         filtered_nni_times = original_nni_times[filtered_indices]
@@ -155,7 +200,7 @@ if __name__ == '__main__':
         # Calculate statistics for filtered NN intervals
         filtered_mean_nni = np.mean(filtered_nn_intervals)
         filtered_std_nni = np.std(filtered_nn_intervals)
-
+        
 
         plt.figure()
 
@@ -185,8 +230,3 @@ if __name__ == '__main__':
 
         plt.tight_layout()
         plt.show()
-
-
-
-    
-    plt.show()
